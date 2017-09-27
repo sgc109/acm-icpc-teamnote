acb11af58c40e81d2c5ad8791f6da67ae09e9c3f
sgc109 <sgc109109@gmail.com>
2017-09-27 10:48:13 +0900

    edited PlainSweep.cpp

---
+++ PlainSweep.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+#define fastio() ios::sync_with_stdio(0),cin.tie(0);
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int inf = 0x3c3c3c3c;
+const ll infl = 0x3c3c3c3c3c3c3c3c;
+
+int N;
+int main(){
+    fastio();
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        int x1, y1, x2, y2;
+    }
+}
