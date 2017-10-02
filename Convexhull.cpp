ce1ad7e07779e5e03883f0f78fa49d52538c5c9a
sgc109 <sgc109109@gmail.com>
2017-10-02 12:48:15 +0900

    edited Convexhull.cpp

--- Convexhull.cpp
+++ Convexhull.cpp
@@ -9,6 +9,12 @@ const int mod = 1e9 + 7;
 const int inf = 0x3c3c3c3c;
 const ll infl = 0x3c3c3c3c3c3c3c3c;
 
+/*
+addPoint 로 점들을 추가하고
+makeConvex 함수를 한번 호출해주면
+멤버 변수인 poly에 컨벡스헐을 구성하는
+점들이 들어있게된다.
+*/
 struct Point{
     ll x, y;
     Point operator+(Point& rhs){
@@ -34,36 +40,44 @@ int ccw(Point a, Point b, Point c){
     cw /= abs(cw);
     return cw;
 }
-vector<Point> makeConvex(vector<Point>& ps){
-    int n = sz(ps);
-    sort(all(ps));
-    Point st = ps[0];
-    ps.erase(ps.begin());
-    auto cmp = [&](Point& a, Point& b){
-        int cw = ccw(a, b, st);
-        if(!cw) (a - st).size() < (b - st).size();
-        return cw > 0;
-    };
-    sort(all(ps), cmp);
+struct Convexhull{
     vector<Point> poly;
-    poly.pb(st);
-    for(int i = 0; i < sz(ps); i++){
-        while(sz(poly) >= 2 && ccw(poly.back(), ps[i], poly[sz(poly) - 2]) <= 0) poly.pop_back();
-        poly.pb(ps[i]);
+    vector<Point> ps;
+    void addPoint(ll x, ll y){
+        ps.pb(Point{x, y});
     }
-    return poly;
-}
+    void makeConvex(){
+        int n = sz(ps);
+        sort(all(ps));
+        Point st = ps[0];
+        ps.erase(ps.begin());
+        auto cmp = [&](Point& a, Point& b){
+            int cw = ccw(a, b, st);
+            if(!cw) (a - st).size() < (b - st).size();
+            return cw > 0;
+        };
+        sort(all(ps), cmp);
+        vector<Point> poly;
+        poly.pb(st);
+        for(int i = 0; i < sz(ps); i++){
+            while(sz(poly) >= 2 && ccw(poly.back(), ps[i], poly[sz(poly) - 2]) <= 0) poly.pop_back();
+            poly.pb(ps[i]);
+        }
+        this->poly = poly;
+    }
+};
 int N;
-vector<Point> ps;
 int main(){
     fastio();
     cin >> N;
+    Convexhull convex;
     for(int i = 0; i < N; i++){
         ll x, y;
         cin >> x >> y;
-        ps.pb(Point{x, y});
+        convex.addPoint(x, y);
     }
-    vector<Point> convex = makeConvex(ps);
-    if(sz(convex) <= 2) return !printf("0");
-    cout << sz(convex);
+    convex.makeConvex();
+    auto poly = convex.poly;
+    if(sz(poly) <= 2) return !printf("0");
+    cout << sz(poly);
 }
