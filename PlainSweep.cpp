4526789e823fac4922b02f8751ea153c3745597d
sgc109 <sgc109109@gmail.com>
2017-10-02 11:38:59 +0900

    edited PlainSweep.cpp

--- PlainSweep.cpp
+++ PlainSweep.cpp
@@ -12,27 +12,28 @@ const ll infl = 0x3c3c3c3c3c3c3c3c;
 struct Rect{
     ll x1, y1, x2, y2;
 };
+struct RNG{
+    ll l, r;
+};
 Rect rs[200003];
-ll tr[1200003], cnt[1200003];
+ll tr[1600003], cnt[1600003];
 vector<pair<int,int> > v;
 vector<ll> ys;
+vector<RNG> rngs;
 int N;
 void update(int l, int r, int nl, int nr, int nd, int v){
-//    printf("nl : %d, nr : %d\n", nl, nr);
-    if(nr <= l || r <= nl) return;
-    if(l <= nl && nr <= r) {
+    if(rngs[nr].l < l || r < rngs[nl].r) return;
+    if(l <= rngs[nl].l && rngs[nr].r <= r) {
         cnt[nd] += v;
-        if(cnt[nd]) tr[nd] = ys[nr] - ys[nl];
-        else tr[nd] = tr[2 * nd] + tr[2 * nd + 1];
+        if(cnt[nd]) tr[nd] = rngs[nr].r - rngs[nl].l;
+        else tr[nd] = (nl == nr ? 0 : tr[2 * nd] + tr[2 * nd + 1]);
         return;
     }
-    if(nl + 1 == nr) return;
     int nm = nl + nr >> 1;
     update(l, r, nl, nm, 2 * nd, v);
-    update(l, r, nm, nr, 2 * nd + 1, v);
-    if(cnt[nd]) tr[nd] = ys[nr] - ys[nl];
-    else tr[nd] = tr[2 * nd] + tr[2 * nd + 1];
-//    printf("tr[%d] : %lld\n", nd, tr[nd]);
+    update(l, r, nm + 1, nr, 2 * nd + 1, v);
+    if(cnt[nd]) tr[nd] = rngs[nr].r - rngs[nl].l;
+    else tr[nd] = (nl == nr ? 0 : tr[2 * nd] + tr[2 * nd + 1]);
 }
 int main(){
     fastio();
@@ -48,21 +49,17 @@ int main(){
     }
     sort(all(ys));
     ys.erase(unique(all(ys)), ys.end());
+    for(int i = 0; i < sz(ys) - 1; i++) rngs.pb(RNG{ys[i], ys[i + 1]});
     sort(all(v));
     ll prv = -1;
     ll ans = 0;
     for(auto p : v){
-//        cout << "tr[1] : " << tr[1] << endl;
         ll cur = p.first;
         int isEnd = p.second < 0;
         int id = abs(p.second);
         ans += (cur - prv) * tr[1];
-        ll l = lower_bound(all(ys), rs[id].y1) - ys.begin();
-        ll r = lower_bound(all(ys), rs[id].y2) - ys.begin();
-//        printf("l : %lld, r : %lld\n", l, r);
-        update(l, r, 0, sz(ys) - 1, 1, isEnd ? -1 : 1);
+        update(rs[id].y1, rs[id].y2, 0, sz(rngs) - 1, 1, isEnd ? -1 : 1);
         prv = cur;
-//        printf("prv : %lld\n", prv);
     }
-    cout << ans;
+    cout << ans << '\n';
 }
