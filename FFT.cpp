cadb3c373213c3b93e6ee73825746ef451a3ab9c
sgc109 <sgc109109@gmail.com>
2017-11-06 16:49:51 +0900

    edited FFT.cpp

--- FFT.cpp
+++ FFT.cpp
@@ -1,15 +1,4 @@
-#include <bits/stdc++.h>
-#define pb push_back
-#define sz(v) ((int)(v).size())
-#define all(v) (v).begin(),(v).end()
-#define fastio() ios::sync_with_stdio(0),cin.tie(0)
-using namespace std;
-typedef long long ll;
 typedef complex<double> base;
-const int mod = 1e9 + 7;
-const int inf = 0x3c3c3c3c;
-const ll infl = 0x3c3c3c3c3c3c3c3c;
-const int MAX_N = 1e4 + 9;
 const double PI = 2.0 * acos(0.0);
 
 /*blog.myungwoo.kr/54*/
