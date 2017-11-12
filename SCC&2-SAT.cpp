f0bd79d58ba9ea50609543a35f1b8439fc7cf64d
sgc109 <sgc109109@gmail.com>
2017-11-12 12:03:18 +0900

    edited 2-SAT comment

--- SCC&2-SAT.cpp
+++ SCC&2-SAT.cpp
@@ -1,8 +1,13 @@
 /*
-
 주의 사항
-1. 그래프 구성해 주고나서 답 구하기 전에 build 먼저 해줘야함.
-2. getAns 로 답구하는데 답 없으면 빈 벡터 반환
+1. 생성자에서 size 는 참/거짓을 알고 싶은 변수의 개수를 넣어줘야하며
+   addEdge 를 할 때에도 구조체 밖에서 먼저 노드 id에 2배를 해주고 NOT을 해준 뒤에
+   인자로 넣어줘야함
+2. 2-CNF 식에 따라 addEdge 로 간선 이어줄 때, 각 절 마다 직접 대칭적으로 2개씩 이어줘야함.
+   즉, a -> b 를 이으면 무조건 그의 대우인 ~b -> ~a 도 이어줘야 한다는 뜻
+3. 그래프 구성해 주고나서 답 구하기 전에 build 먼저 해줘야함.
+4. getAns 로 답구하는데 답 없으면 빈 벡터 반환
+5. getCompGraph 는 scc 노드 압축이 필요한 경우가 아니라면 안써도 됨
 */
 
 struct SCC{
@@ -30,9 +35,9 @@ struct SCC{
 	int dfs(int cur){
 		stck.push(cur);
 		int ret = order[cur] = visitCnt++;
-		for(int next : G[cur]){
-			if(order[next] == -1) ret = min(ret, dfs(next));
-			else if(sccId[next] == -1) ret = min(ret, order[next]);
+		for(int nxt : G[cur]){
+			if(order[nxt] == -1) ret = min(ret, dfs(nxt));
+			else if(sccId[nxt] == -1) ret = min(ret, order[nxt]);
 		}
 		if(order[cur] == ret){
 			while(1){
@@ -45,12 +50,13 @@ struct SCC{
 		}
 		return ret;
 	}
+    // 노드 압축이므로 필요하지 않을 경우 안써도 됨
 	vector<vector<int> > getCompGraph(){
 		vector<vector<int> > compG = vector<vector<int> >(sccCnt);
 		for(int cur = 0 ; cur < size; cur++){
-			for(int next : G[cur]){
-				if(sccId[cur] == sccId[next]) continue;
-				compG[sccId[cur]].push_back(sccId[next]);
+			for(int nxt : G[cur]){
+				if(sccId[cur] == sccId[nxt]) continue;
+				compG[sccId[cur]].push_back(sccId[nxt]);
 			}
 		}
 		sort(compG.begin(), compG.end());
@@ -64,17 +70,18 @@ struct TwoSAT{
 	int size;
 	SCC scc;
 	vector<int> ans;
-	bool isPossible;
+	bool isPoss;
 	TwoSAT(int size){
+        size *= 2;
 		this->size = size;
 		scc = SCC(size);
 	}
 	void buildTwoSAT(){
 		scc.buildSCC();
 		vector<int> sccId = scc.sccId;
-		isPossible = true;
+		isPoss = true;
 		for(int i = 0 ; i < size; i+=2){
-			if(sccId[i] == sccId[i+1]) isPossible = false;
+			if(sccId[i] == sccId[i+1]) isPoss = false;
 		}
 		ans = vector<int>(size / 2, -1);
 		for(int i = 0; i < size; i += 2){
@@ -85,7 +92,7 @@ struct TwoSAT{
 		scc.addEdge(from, to);
 	}
 	vector<int> getAns(){
-		if(!isPossible) return vector<int>();
+		if(!isPoss) return vector<int>();
 		return ans;
 	}
 };
